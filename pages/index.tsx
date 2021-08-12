77724641f96d59e0113122d6ebed95b4e4c5188a
Ronald Skinner <rjskinnerc@unal.edu.co>
2021-08-12 11:51:24 -0500

    Install TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Cashflow by Dev2Cloud</title>
-        <meta name="description" content="Cashflow tracking app by Dev2Cloud" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Cashflow App by Dev2Cloud
-        </h1>
-      </main>
+    <div>
+      <h1>Home Page</h1>
     </div>
   )
 }
