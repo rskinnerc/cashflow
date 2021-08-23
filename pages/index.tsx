0587718c16216737475144d16299ba693f67f0c7
Ronald Skinner <rjskinnerc@unal.edu.co>
2021-08-23 09:42:41 -0500

    Add Index styles - Implement btn class in global.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
 import { ReactElement } from "react";
 import Link from "next/link";
 import AuthContextProvider from "../store/AuthContextProvider";
 import Navbar from "../components/Navbar";
+import { useContext } from "react";
+import AuthContext from "../store/AuthContext";
 
 const Home: NextPageWithLayout = () => {
-  return (
-    <div>
-      <h1 className="font-semibold text-2xl">Home Page</h1>
+  const { user } = useContext(AuthContext);
 
-      <hr />
-      <Link href="/balances?new=true">Balances</Link>
-      <Link href="/pockets?new=true">Pockets</Link>
-      <Link href="/auth">Auth</Link>
-    </div>
+  // TODO: Implement No User component
+  if (!user) {
+    return <h2>No User</h2>;
+  }
+
+  return (
+    <>
+      <div className="row-span-1 flex flex-col items-center my-5">
+        <img
+          src={user.photoURL!}
+          alt={user.displayName!}
+          className="ring-2 rounded-full ring-gray-200 sm:w-28 w-24 mb-5"
+        ></img>
+
+        <h3 className="text-lg">Welcome Back</h3>
+        <h1 className="text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-br from-gray-800 to-blue-500">
+          {user!.displayName!}
+        </h1>
+        <h3 className="italic font-light">{user!.email}</h3>
+      </div>
+      <div className="row-span-1 mt-10 flex flex-col items-center justify-start sm:gap-2 gap-2">
+        <Link href="/balances?new=true">
+          <a className="btn">Add Transaction</a>
+        </Link>
+        <Link href="/pockets?new=true">
+          <a className="btn">New Pocket</a>
+        </Link>
+      </div>
+    </>
   );
 };
 
@@ -20,7 +45,7 @@ Home.getLayout = function getLayout(page: ReactElement) {
   return (
     <AuthContextProvider guest={false}>
       <Navbar />
-      {page}
+      <div className="grid grid-cols-1 grid-rows-1 m-5">{page}</div>
     </AuthContextProvider>
   );
 };
